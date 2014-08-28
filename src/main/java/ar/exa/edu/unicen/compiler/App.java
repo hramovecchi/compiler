0bd29f2f2e87e838d6496d22d692a1d98c9c34b1
Pablo Martín Villafañe <pmvillafane@aveise.com>
2014-08-28 19:06:11 -0300

    Initial project version

---
+++ src/main/java/ar/exa/edu/unicen/compiler/App.java
@@ -0,0 +1,13 @@
+package ar.exa.edu.unicen.compiler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
