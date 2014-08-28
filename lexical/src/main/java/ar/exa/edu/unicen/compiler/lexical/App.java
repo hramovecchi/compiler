4f60fba021fc4a58618c9838e66e6e34e4056c2c
Pablo Martín Villafañe <pmvillafane@aveise.com>
2014-08-28 19:23:02 -0300

    Project modules for: Lexical, Syntactic and Code Generator

---
+++ lexical/src/main/java/ar/exa/edu/unicen/compiler/lexical/App.java
@@ -0,0 +1,13 @@
+package ar.exa.edu.unicen.compiler.lexical;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
