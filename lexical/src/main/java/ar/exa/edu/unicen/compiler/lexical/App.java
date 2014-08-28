46a953e7e64957a03ff15d906b889065fc1c14b1
Pablo Martín Villafañe <pmvillafane@aveise.com>
2014-08-28 20:35:16 -0300

    Java Apps

--- lexical/src/main/java/ar/exa/edu/unicen/compiler/lexical/App.java
+++ lexical/src/main/java/ar/exa/edu/unicen/compiler/lexical/App.java
@@ -2,12 +2,10 @@ package ar.exa.edu.unicen.compiler.lexical;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
